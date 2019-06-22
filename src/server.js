361e1d9204c9fee194dfb129ca13a3dc039c785e
13akstjq <13akstjq@naver.com>
2019-06-22 19:37:45 +0900

    prisma setting finish

--- src/server.js
+++ src/server.js
@@ -1,22 +1,28 @@
 require("dotenv").config()
+import logger from 'morgan';
 import {GraphQLServer} from 'graphql-yoga';
-
+import schema from './schema';
 const PORT = process.env.PORT || 4000;
 
 //typeDefs
-const typeDefs = `
-    type Query{
-        hello : String!
-    }
-`
+// const typeDefs = `
+//     type Query{
+//         hello : String!
+//     }
+// `
 
 //resolvers
-const resolvers = {
-    Query : {
-        hello : () => "hello"
-    }
-}
+// const resolvers = {
+//     Query : {
+//         hello : () => "hello"
+//     }
+// }
 
-const server = new GraphQLServer({typeDefs,resolvers});
+// type , resolvers를 server.js에서 선언한 버전
+// const server = new GraphQLServer({typeDefs,resolvers});
 
-server.start({port : PORT}, () => console.log("server is running http://localhost:"));
+// schema가 api를 합친 버전
+const server = new GraphQLServer({schema});
+server.express.use(logger("dev"));
+
+server.start({port : PORT}, () => console.log(`server is running http://localhost:${PORT}`));
